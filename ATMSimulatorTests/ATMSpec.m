5d63b728b85e00f658d90ded4a690be08bc83f93
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-03 11:23:13 +0100

    Red: Refactored 'sut' to 'atm' for clarity, added expection that the ATM has a console with an initial message

--- ATMSimulatorTests/ATMSpec.m
+++ ATMSimulatorTests/ATMSpec.m
@@ -5,26 +5,35 @@ SPEC_BEGIN(ATMSpec)
 
 describe(@"ATM", ^{
     
-    __block ATM *sut;
+    __block ATM *atm;
+    __block id console;
     
     beforeEach(^{
         
-        sut = [ATM new];
+        atm = [ATM new];
+        atm.console = console = [KWMock mockForProtocol:@protocol(ATMConsole)];
         
     });
     
     afterEach(^{
         
-        sut = nil;
+        atm = nil;
         
     });
     
     it(@"should exist", ^{
         
-        [sut shouldNotBeNil];
+        [atm shouldNotBeNil];
         
     });
     
+    it(@"should show 'not available' on console when powered off", ^{
+       
+        [[console should] receive:@selector(message) andReturn: @"Not Available"];
+        
+        [[[atm.console message] should] equal: @"Not Available"];
+        
+    });
     
 });
 
