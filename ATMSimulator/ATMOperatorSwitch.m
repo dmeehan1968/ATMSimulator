4e8d911ce193764705c227ab866d9a86033ec9f4
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-04 17:55:18 +0100

    Green: Added notification of delegate when state changes

--- ATMSimulator/ATMOperatorSwitch.m
+++ ATMSimulator/ATMOperatorSwitch.m
@@ -2,5 +2,26 @@
 #import "ATMOperatorSwitch.h"
 
 @implementation ATMOperatorSwitch
+{}
 
+#pragma mark - State
+
+-(void)setState:(BOOL)state {
+	
+	_state = state;
+	
+	[self didChangeToState: _state];
+	
+}
+
+#pragma mark - Delegate Helpers
+
+-(void)didChangeToState: (BOOL) state {
+	
+	if ([self.delegate respondsToSelector:@selector(operatorSwitch:didChangeToState:)]) {
+		
+		[self.delegate operatorSwitch:self didChangeToState:state];
+	}
+	
+}
 @end
