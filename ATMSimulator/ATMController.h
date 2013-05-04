eeed41115ee8a285352bfa50a0969667b943ea5a
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-04 15:51:34 +0100

    Green: Initial implemenation of ATMController

---
+++ ATMSimulator/ATMController.h
@@ -0,0 +1,13 @@
+//
+//  ATMController.h
+//  ATMSimulator
+//
+//  Created by Dave Meehan on 04/05/2013.
+//  Copyright (c) 2013 Dave Meehan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATMController : NSObject
+
+@end
---
+++ ATMSimulator/ATMController.m
@@ -0,0 +1,13 @@
+//
+//  ATMController.m
+//  ATMSimulator
+//
+//  Created by Dave Meehan on 04/05/2013.
+//  Copyright (c) 2013 Dave Meehan. All rights reserved.
+//
+
+#import "ATMController.h"
+
+@implementation ATMController
+
+@end
