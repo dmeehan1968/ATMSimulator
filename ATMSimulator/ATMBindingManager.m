2472d89e818ef25056ffa8f8b26128fb1c1f98f2
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-06 00:08:33 +0100

    Green: Implemented ability to create binding that transfers value from one object to another

--- ATMSimulator/ATMBindingManager.m
+++ ATMSimulator/ATMBindingManager.m
@@ -8,10 +8,73 @@
 
 #import "ATMBindingManager.h"
 
+@interface ATMBinding : NSObject
+
+@property (weak) id observer;
+@property (strong, nonatomic) NSString *observerKeypath;
+@property (weak) id subject;
+@property (strong, nonatomic) NSString *subjectKeypath;
+
+-(void)enable;
+-(void)disable;
+
+@end
+
+@implementation ATMBinding
+
+-(void)enable {
+
+	[self.subject addObserver:self forKeyPath:self.subjectKeypath options:NSKeyValueObservingOptionNew context:nil];
+}
+
+-(void)disable {
+
+	[self.subject removeObserver:self forKeyPath:self.subjectKeypath context:nil];
+	
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	
+	id newValue = [change valueForKey: NSKeyValueChangeNewKey];
+	
+	NSLog(@"New Value: %@", newValue);
+	
+	[self.observer setValue:newValue forKey:self.observerKeypath];
+	
+}
+
+@end
+
+@interface ATMBindingManager ()
+
+@property (strong, nonatomic) NSArray *bindings;
+
+@end
+
 @implementation ATMBindingManager
+
+-(id)init {
+	if (self = [super init]) {
+		_bindings = @[];
+	}
+	return self;
+}
 
 -(void)bind:(id)observer keypath:(NSString *)observerKeypath subject:(id)subject keypath:(NSString *)subjectKeypath {
 	
+	ATMBinding *binding = [ATMBinding new];
+	
+	binding.observer = observer;
+	binding.observerKeypath = observerKeypath;
+	binding.subject = subject;
+	binding.subjectKeypath = subjectKeypath;
+
+	self.bindings = [self.bindings arrayByAddingObject:binding];
+	
+	[binding enable];
 }
+
+
 
 @end
