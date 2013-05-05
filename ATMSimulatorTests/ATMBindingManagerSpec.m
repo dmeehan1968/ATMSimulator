bbb27f40366185d6773b701208646d5bc85d7c46
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-05 23:38:18 +0100

    Red: Add expectation that a binding can be made

--- ATMSimulatorTests/ATMBindingManagerSpec.m
+++ ATMSimulatorTests/ATMBindingManagerSpec.m
@@ -1,15 +1,39 @@
 #import "Kiwi.h"
 #import "ATMBindingManager.h"
 
+@interface ObjectA : NSObject
+
+@property (strong, nonatomic) NSString *aStringValue;
+
+@end
+
+@implementation ObjectA
+
+@end
+
+@interface ObjectB : NSObject
+
+@property (strong, nonatomic) NSString *bStringValue;
+
+@end
+
+@implementation ObjectB
+
+@end
+
 SPEC_BEGIN(ATMBindingManagerSpec)
 
 describe(@"ATMBindingManager", ^{
 	
     __block ATMBindingManager *sut;
+	__block ObjectA *objectA;
+	__block ObjectB *objectB;
     
     beforeEach(^{
 		
         sut = [[ATMBindingManager alloc] init];
+		objectA = [ObjectA new];
+		objectB = [ObjectB new];
         
     });
     
@@ -23,6 +47,25 @@ describe(@"ATMBindingManager", ^{
         [sut shouldNotBeNil];
         
     });
+	
+	it(@"should allow a binding to be made between two objects", ^{
+		
+		[sut bind: objectA keypath: @"aStringValue" subject: objectB keypath: @"bStringValue"];
+		
+	});
+
+	xit(@"should create a one-way binding, setting objectB.bStringValue should update objectA.aStringValue", ^{
+		
+		NSString *expectedResult = @"Test Value";
+		
+		[sut bind: objectA keypath: @"aStringValue" subject: objectB keypath: @"bStringValue"];
+		
+		objectB.bStringValue = expectedResult;
+		
+		[[objectA.aStringValue should] equal:expectedResult];
+		
+	});
+
     
 });
 
