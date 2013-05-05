eeaa6a41466c93a4210042ce90c08fa4419dbb39
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-05 23:28:41 +0100

    Green: Initial implementation of ATMBindingManager

---
+++ ATMSimulator/ATMBindingManager.h
@@ -0,0 +1,13 @@
+//
+//  ATMBindingManager.h
+//  ATMSimulator
+//
+//  Created by Dave Meehan on 05/05/2013.
+//  Copyright (c) 2013 Dave Meehan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATMBindingManager : NSObject
+
+@end
---
+++ ATMSimulator/ATMBindingManager.m
@@ -0,0 +1,13 @@
+//
+//  ATMBindingManager.m
+//  ATMSimulator
+//
+//  Created by Dave Meehan on 05/05/2013.
+//  Copyright (c) 2013 Dave Meehan. All rights reserved.
+//
+
+#import "ATMBindingManager.h"
+
+@implementation ATMBindingManager
+
+@end
