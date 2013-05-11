229e23732ccc22f2c3d8da9705fced42e70856b1
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-11 17:20:47 +0100

    Green: Track last input event

--- ATMSimulator/ATMConsole.h
+++ ATMSimulator/ATMConsole.h
@@ -3,9 +3,13 @@
 
 @property (strong, nonatomic) NSString *message;
 @property (strong, nonatomic) NSArray *inputOptions;
+@property (assign, nonatomic) NSInteger lastInputReceived;
+
+-(void)didSelectInputOption: (NSInteger) index;
 
 @end
 
 @interface ATMConsole : NSObject <ATMConsole>
+
 
 @end
--- ATMSimulator/ATMConsole.m
+++ ATMSimulator/ATMConsole.m
@@ -5,15 +5,25 @@
 
 @synthesize message = _message;
 @synthesize inputOptions = _inputOptions;
+@synthesize lastInputReceived = _lastInputReceived;
 
 -(id)init {
 
 	if (self = [super init]) {
 		_message = @"";
 		_inputOptions = @[];
+		_lastInputReceived = NSNotFound;
 	}
 	
 	return self;
+	
+}
+
+-(void)didSelectInputOption:(NSInteger)index {
+	
+	NSParameterAssert(index < self.inputOptions.count);
+	
+	self.lastInputReceived = index;
 	
 }
 
